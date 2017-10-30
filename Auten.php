59407ff1aa6db6dfed811eb6a8da07d93b9ce616
cristian1027 <cristian.a.17@hotmail.com>
2017-10-30 09:52:47 -0500

    php

---
+++ Auten.php
@@ -0,0 +1,32 @@
+<?php
+ 
+  $cedula=$_POST['Ced'];
+  $Contraseña=$_POST['Con'];
+  
+
+  echo "$cedula\n";
+  echo "$Contraseña";
+
+/*$Cedulas = array("qwer","abcd","poiu");
+$Contra= array("0000","1234","9876");
+
+
+for ($i=0; $i < count($Contra); $i++) { 
+
+	if ("$cedula"==$Cedulas[$i]) {
+		if ("$Contraseña"==$Contra[$i])
+			 {
+			 	//echo "hola";
+			 	header("Location: ../Frontend/reservaSillas.html");
+			
+		}
+		else{
+			header("Location: ../Frontend/reservaSillas.html");
+		}
+	}
+	else{
+		header("Location: ../Frontend/reservaSillas.html");
+	}
+}*/
+
+?>
